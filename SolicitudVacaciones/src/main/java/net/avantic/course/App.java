202485b0f24522d8043ee484a6b90104956f08b8
Yeray Darias <ydarias@gmail.com>
2012-10-08 13:47:29 +0100

    Hola mundo haciendo uso de Spring

--- SolicitudVacaciones/src/main/java/net/avantic/course/App.java
+++ SolicitudVacaciones/src/main/java/net/avantic/course/App.java
@@ -1,5 +1,8 @@
 package net.avantic.course;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    	HelloWorld helloWorldInstance = (HelloWorld) context.getBean("helloWorldBean");
+    	helloWorldInstance.printHello();
     }
 }
